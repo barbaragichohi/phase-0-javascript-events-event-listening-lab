8b2fffadc2847f3dfd480194b96e9c5c64caf778
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-22 20:43:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    alert('I was clicked!')
+    input.addEventListener('click', addingEventListener);
 }
+
+// grab the node we want to create addingEventListener()
+const input = document.getElementById("input");
+
+    
+
+
+// create the event listener
+
